2f64c3db148ed0e7ae1463d2813f02af8d9a3f13
Nicolas Villarreal <105127312+Nico-Villarreal@users.noreply.github.com>
2022-11-13 04:41:52 -0300

    entrega final v1.1

---
+++ js/carrito.js
@@ -0,0 +1,31 @@
+const formCompra = document.getElementById('procesar-pago')
+
+activarFuncion.addEventListener('click', procesarCompra)
+
+formCompra.addEventListener('submit', enviarPedido)
+
+function enviarPedido(e){
+    e.preventDefault()
+
+    const cliente = document.getElementById('cliente').value
+    const correo = document.getElementById('correo').value
+
+    if(correo === "" || cliente === ""){
+        swal("Debes completar los Campos vacios", "Rellena el formulario", "error");
+    }else{
+        const spinenr = document.getElementById('spinner')
+        spinenr.classList.add("d-flex")
+        spinenr.classList.remove("d-none")
+
+        setTimeout (() => {
+            const spinenr = document.getElementById('spinner');
+            spinenr.classList.remove("d-flex");
+            spinenr.classList.add("d-none");
+            formCompra.reset();
+        }, 3500)
+
+    }
+
+}
+
+
--- js/index.js
+++ js/index.js
@@ -2,36 +2,16 @@
 //para esto voy a comenzar con lo basico visto en clase que es logearse, agregar productos y posteriormente realizar una suma de los procustos//
 
 
-//funcion constructora y agregando base de datos local de los productos para iteraccionar con ellos
-class producto {
-    constructor (id, nombre, tipo, calibre, capacidad, precio, img, cantidad, categoria){
-        this.id = id;
-        this.nombre = nombre.toUpperCase();
-        this.tipo = tipo;
-        this.calibre = calibre;
-        this.capacidad = capacidad;
-        this.precio = parseFloat(precio);
-        this.img = img;
-        this.cantidad = cantidad
-        this.categoria = categoria
-    }
-}
-
-const catalogo = [];
-catalogo.push(new producto(1, "HK-USP", "Semiautomatica", "9mm", "16 mun.", 3500, "HK-USP.webp", 1, "Pistolas y Revolveres"));
-catalogo.push(new producto(2, "DESERT EAGLE", "Semiautomatica", ".45 mm", "7 mun", 5500, "DESERT-EAGLE.webp", 1, "Pistolas y Revolveres"));
-catalogo.push(new producto(3, "MAGNUM .44", "Semiautomatica", ".44 mm", "6 mun. (Tambor)", 5900, "MAGNUM-44.webp", 1, "Pistolas y Revolveres"));
-catalogo.push(new producto(4, "GLOCK 17", "Semiautomatica", "9mm", "17 mun.", 4800, "GLOCK-17.webp", 1, "Pistolas y Revolveres"));
-
-
-
 //constantes para selecconar ID del HTML
 
-const containerCarrito = document.getElementById('container-Carrito');
 const botonVaciar = document.getElementById('vaciar-carrito')
 const contadorCarrito = document.getElementById('contadorCarrito')
 const precioTotal = document.getElementById('precioTotal')
 const finalizarCompra = document.getElementById('finalizar-compra')
+const activarFuncion = document.getElementById('activarFuncion')
+const containerCarrito = document.getElementById('container-Carrito');
+const divArticle = document.getElementById ('divArticle');
+
 
 let carrito = []
 
@@ -40,21 +20,25 @@ let carrito = []
 document.addEventListener('DOMContentLoaded', () => {
     carrito = JSON.parse(localStorage.getItem('carrito')) || [];
     actualizarCarrito();
+
+    if(activarFuncion){
+        document.querySelector('#activarFuncion').click(procesarCompra);
+    }
+
 })
 
 
 // agregando productos en cards con innerHTML
 
-const divArticle = document.getElementById ('divArticle');
-
-catalogo.forEach(producto => {
+if(divArticle){
+    catalogo.forEach(producto => {
     let article = document.createElement('article');
     article.classList.add('section1-catalogo__article', 'd-flex', 'flex-column', 'm-2');
     const {id, nombre, tipo, calibre, capacidad, precio, img} = producto
 
     article.innerHTML = 
                        `<div class="section1-catalogo__article-1--div1 div-article__active">
-                            <img src="./img/${img}" alt="HK-USP" class="section1-catalogo__article1 article1__div1-img border border-dark rounded-3">
+                            <img src="./img/${img}" alt="${nombre}" class="section1-catalogo__article1 article1__div1-img border border-dark rounded-3">
                         </div>
                         <div class="section1-catalogo__article-1--div2 d-flex flex-column justify-content-center border border-dark rounded-3 pb-2">
                             <p class="section1-catalogo__article-1--div2--text justify-content-center m-1">
@@ -68,18 +52,19 @@ catalogo.forEach(producto => {
                             <button type="button" id="agregar${id}" class="btn btn-secondary m-3">AGREGAR</button>
                         </div>`
 
-    divArticle.appendChild(article); 
+        divArticle.appendChild(article); 
     
 //agregando evento al tag "button" de agregar al carrito
 
-    const boton = document.getElementById(`agregar${id}`)
+        const boton = document.getElementById(`agregar${id}`)
 
-    boton.addEventListener('click', () => {
-        agregarCarrito(id);
+        boton.addEventListener('click', () => {
+            agregarCarrito(id);
+        })
+    
     })
     
-})
-
+}
 
 //let para agregar productos con el tag "button" al carrito
 const agregarCarrito = (prodId) => {
@@ -99,40 +84,77 @@ const agregarCarrito = (prodId) => {
 }
 
 //boton de vaciar productos del carrito 
- 
-botonVaciar.addEventListener ('click', () => {
-    carrito.length = [];
-    actualizarCarrito ();
-})
+if(botonVaciar){
+    botonVaciar.addEventListener ('click', () => {
+        swal({
+            title: "Desea eliminar los productos",
+            text: "Una vez eliminados deberas agregarlos nuevamente",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete ) => {
+            
+            if (willDelete) {
+              swal("Productos eliminados correctamente", {
+                icon: "success",
+              });
+              carrito.length = [];
+            } else {
+              swal("Continua con tu compra");
+            }
+          });
+    
+    
+          actualizarCarrito ();
+    })
+}
 
+//boton para finalizar compra e ir para la pagina de compra
+if(finalizarCompra){
+    finalizarCompra.addEventListener('click', () =>{
+        if (carrito.length === 0) {
+            swal("Espera!", "Tu carrito esta Vacio", "warning");;
+          } else {
+            location.href = "carrito.html";
+            procesarCompra();
+          }
+    })
+}
 
 //Actuaizando el carrito nuevamente seteando el div colocando uno nuevo
+
 const actualizarCarrito = () => {
-    containerCarrito.innerHTML = " ";
+    if(containerCarrito){
+        containerCarrito.innerHTML = " ";
+        carrito.forEach((producto) => {
+            const div = document.createElement('div');
+            div.className = (`productoEnCarrito text-white mt-5 rounded-2 d-flex`);
+            const {id, nombre, precio, cantidad, img} = producto;
+            div.innerHTML += `
+                            <div> 
+                                <img src="./img/${img}" alt="${nombre}" class="section1-catalogo__article1 article1__div1-img img-fluid border border-dark rounded-3 w-75">
+                            </div>
+                            <div> 
+                                <p class="border border-light border-2 p-2 rounded-2 mt-2"> ${nombre}</p>
+                                <p class="border border-light border-2 p-2 rounded-2"> Precio: $ ${precio}</p>
+                                <p class="border border-light border-2 p-2 rounded-2"> Cantidad: <span id="cantidad"> ${cantidad}</p>
+                                <button onclick = "eliminarProducto(${id})" class="btn btn-danger boton-eliminar p-2 rounded-2 border border-white">Eliminar producto</button>
+                            </div>
+                            `
+            containerCarrito.appendChild(div);
+        })
+    }
 
-    carrito.forEach((producto) => {
-        const div = document.createElement('div');
-        div.className = (`productoEnCarrito text-white mt-5 rounded-2 d-flex`);
-        const {id, nombre, precio, cantidad, img} = producto;
-        div.innerHTML += `
-                        <div> 
-                            <img src="./img/${img}" alt="HK-USP" class="section1-catalogo__article1 article1__div1-img img-fluid border border-dark rounded-3 w-75">
-                        </div>
-                        <div> 
-                            <p class="border border-light border-2 p-2 rounded-2 mt-2"> ${nombre}</p>
-                            <p class="border border-light border-2 p-2 rounded-2"> Precio: $ ${precio}</p>
-                            <p class="border border-light border-2 p-2 rounded-2"> Cantidad: <span id="cantidad"> ${cantidad}</p>
-                            <button onclick = "eliminarProducto(${id})" class="btn btn-danger boton-eliminar p-2 rounded-2 border border-white">Eliminar producto</button>
-                        </div>
-                        `
-        containerCarrito.appendChild(div);     
-        
-
-    })
-
+    if(carrito.length === 0){
+        containerCarrito.innerHTML= `<p class="fs-4 fw-bolder text-center ">No hay productos agregados</p>`
+    }
 
     contadorCarrito.textContent = carrito.length;
-    precioTotal.textContent = carrito.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0);
+
+    if(precioTotal){
+        precioTotal.textContent = carrito.reduce((acc, producto) => acc + producto.cantidad * producto.precio, 0);
+    }
 
     guardarEnStorage ()
 
@@ -148,4 +170,29 @@ function eliminarProducto(prodId) {
 //funcion para guardar en el LocalStorage
 function guardarEnStorage (){
     localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+
+function procesarCompra() {
+    carrito.forEach((producto) => {
+    const listaCompra = document.querySelector("#tbody");
+    const {nombre, precio, img, cantidad } = producto;
+
+        const row = document.createElement("tr");
+        row.innerHTML += `
+                        <td>
+                        <img class="img-fluid w-25 border border-dark rounded-2" src="./img/${img}"/>
+                        </td>
+                        <td>${nombre}</td>
+                        <td>${precio}</td>
+                        <td>${cantidad}</td>
+                        <td>${precio * cantidad}</td>
+                        `;
+        listaCompra.appendChild(row);
+    });
+
+    totalProceso.innerText = carrito.reduce(
+      (acc, prod) => acc + prod.cantidad * prod.precio,
+      0
+    );
 }
